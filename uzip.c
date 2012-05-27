9f0d1cb777e941b1b39958225ceb14ae3c10aad8
Dave Vasilevsky <dave@vasilevsky.ca>
2012-05-26 21:21:04 -0400

    Read uzip successfully!

--- uzip.c
+++ uzip.c
@@ -1,18 +1,37 @@
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
 
+#include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <libgen.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include <zlib.h>
+
+static const char uzip_suf[] = ".uzip";
+static const char uzip_magic[] = "#!/bin/sh\n#V";
+static const size_t uzip_header_offset = 128;
+
+typedef struct {
+	uint32_t blocksize;
+	uint32_t blocks;
+} uzip_header;
+
 typedef struct {
 	int fd;
 	char *name;
+	uzip_header header;
+	uint64_t *offsets;
 } uzip;
+
+static uint64_t uzip_ntohll(uint64_t n) {
+	return ((uint64_t)ntohl(n) << 32) | (uint32_t)ntohl(n >> 32);
+}
 
 static void *uzip_init(struct fuse_conn_info *conn) {
 	return fuse_get_context()->private_data;
@@ -22,6 +41,7 @@ static void uzip_destroy(void *data) {
 	uzip *u = data;
 	close(u->fd);
 	free(u->name);
+	free(u->offsets);
 }
 
 static int uzip_getattr(const char *path, struct stat *st) {
@@ -32,7 +52,8 @@ static int uzip_getattr(const char *path, struct stat *st) {
 		st->st_nlink = 2;
 	} else if (path[0] == '/' && strcmp(path + 1, u->name) == 0) {
 		int err = fstat(u->fd, st);
-		// FIXME: size
+		st->st_size = u->header.blocks * u->header.blocksize;
+		st->st_blksize = u->header.blocksize;
 		return err;
 	} else {
 		return -ENOENT;
@@ -52,11 +73,91 @@ static int uzip_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
+static int uzip_open(const char *path, struct fuse_file_info *fi) {
+	uzip *u = fuse_get_context()->private_data;
+	if (path[0] != '/' || strcmp(path + 1, u->name) != 0)
+		return -ENOENT;
+	if ((fi->flags & O_RDWR) || (fi->flags & O_WRONLY))
+		return -EACCES;
+	return 0;
+}
+
+static char *uzip_block(uzip *u, size_t n) {
+	if (n > u->header.blocks) {
+		fprintf(stderr, "Invalid block\n");
+		return NULL;
+	}
+	
+	size_t size = u->offsets[n+1] - u->offsets[n];
+	char *comp = NULL, *ucomp = NULL;
+	if (!(ucomp = calloc(1, u->header.blocksize))) {
+		fprintf(stderr, "Can't allocate block memory\n");
+		goto error;
+	}
+	if (size == 0)
+		return ucomp;
+	
+	if (!(comp = malloc(size))) {
+		fprintf(stderr, "Can't allocate block data\n");
+		goto error;
+	}
+	if (pread(u->fd, comp, size, u->offsets[n]) != size) {
+		perror("Reading block");
+		goto error;
+	}
+	
+	uLongf dlen = u->header.blocksize;
+	if (uncompress((Bytef*)ucomp, &dlen, (Bytef*)comp, size) != Z_OK) {
+		fprintf(stderr, "Decompression error\n");
+		goto error;
+	}
+	
+	free(comp);
+	return ucomp;
+	
+error:
+	free(comp);
+	free(ucomp);
+	return NULL;
+}
+
+static int uzip_read(const char *path, char *buf, size_t size, off_t offset,
+		struct fuse_file_info *fi) {
+	uzip *u = fuse_get_context()->private_data;
+	if (path[0] != '/' || strcmp(path + 1, u->name) != 0)
+		return -ENOENT;
+	
+	size_t read = 0;
+	while (size > 0) {
+		size_t block = offset / u->header.blocksize;
+		if (block >= u->header.blocks)
+			break;
+		
+		size_t off = offset % u->header.blocksize;
+		size_t to_read = u->header.blocksize - off;
+		if (size < to_read)
+			to_read = size;
+		
+		size_t csize = u->offsets[block+1] - u->offsets[block];
+		if (csize > 0) {
+			char *data = uzip_block(u, block);
+			if (!data)
+				return -EIO;
+			memcpy(buf + read, data + off, to_read);
+		}
+		
+		read += to_read;
+		offset += to_read;
+		size -= to_read;
+	}
+	return read;
+}
+
 static struct fuse_operations uzip_ops = {
 	.getattr	= uzip_getattr,
 	.readdir	= uzip_readdir,
-	// .open		= uzip_open,
-	// .read		= uzip_read,
+	.open		= uzip_open,
+	.read		= uzip_read,
 	.init		= uzip_init,
 	.destroy	= uzip_destroy,
 };
@@ -72,10 +173,6 @@ static int uzip_opt_proc(void *data, const char *arg, int key,
 	}
 	return 1;
 }
-
-static const char uzip_suf[] = ".uzip";
-
-static const char uzip_magic[] = "#!/bin/sh\n#V";
 
 int main(int argc, char *argv[]) {
 	char *file = NULL;
@@ -108,8 +205,30 @@ int main(int argc, char *argv[]) {
 	}
 	if (magic[sizeof(uzip_magic) - 1] != '2') {
 		fprintf(stderr, "Bad uzip version\n");
+		return -1;
+	}
+	
+	if (lseek(u.fd, uzip_header_offset, SEEK_SET) == -1) {
+		perror("Seeking to header");
+		return -1;
+	}
+	if (read(u.fd, &u.header, sizeof(u.header)) != sizeof(u.header)) {
+		perror("Reading header");
 		return -1;		
 	}
+	u.header.blocksize = ntohl(u.header.blocksize);
+	u.header.blocks = ntohl(u.header.blocks);
+	size_t offsize = (u.header.blocks + 1) * sizeof(uint64_t);
+	if (!(u.offsets = malloc(offsize))) {
+		fprintf(stderr, "Out of memory allocating blocks\n");
+		return -1;
+	}
+	if (read(u.fd, u.offsets, offsize) != offsize) {
+		perror("Reading offsets");
+		return -1;		
+	}
+	for (size_t i = 0; i <= u.header.blocks; ++i)
+		u.offsets[i] = uzip_ntohll(u.offsets[i]);
 	
 	return fuse_main(args.argc, args.argv, &uzip_ops, &u);
 }
